b141c1a566016290e4a430cf51350844a3a115c7
John P. rouillard <rouilj+github@ieee.org>
2024-05-21 14:29:06 -0400

    refactor: assign tr first then use it when assigning id rather than repeating object chain

--- html/classhelper.js
+++ html/classhelper.js
@@ -852,14 +852,14 @@ class ClassHelper extends HTMLElement {
             tbody.addEventListener("click", (e) => {
                 let id, tr;
                 if (e.target.tagName === "INPUT" ) {
-                  tr = e.target.parentElement.parentElement;
-		  id = tr.dataset.id;
+                   tr = e.target.parentElement.parentElement;
+                   id = tr.dataset.id;
 	        } else if (e.target.tagName === "TD") {
-                    id = e.target.parentElement.dataset.id;
-                    tr = e.target.parentElement;
+                   tr = e.target.parentElement;
+                   id = tr.dataset.id;
                 } else if (e.target.tagName === "TR") {
-                    id = e.target.dataset.id;
-                    tr = e.target;
+                   tr = e.target;
+                   id = tr.dataset.id;
                 }
 
               if (e.target.tagName !== "INPUT") {
